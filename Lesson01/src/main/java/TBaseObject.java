ad864f612a48421f6fe053350d32271543d8a3c8
ososov <ososov@mail.ru>
2020-07-05 10:10:31 +0300

    createLesson01

---
+++ Lesson01/src/main/java/TBaseObject.java
@@ -0,0 +1,16 @@
+/**
+ * @author Ososov
+ * Created 04.07.2020
+ */
+public class TBaseObject {
+    private long id;
+    private String caption;
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
+}
